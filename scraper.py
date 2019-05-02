51adbd1db55406494d6cf24f79d61deacca98110
Timothy Ross <tjthejuggler@gmail.com>
2019-05-02 08:21:36 -0400

    added ball num property, hardcoded properties that were coming in messed up

--- scraper.py
+++ scraper.py
@@ -24,8 +24,16 @@ num_parsed_tricks = 0
 all_tricks = {}
 num_tricks = 200
 f = open("./trick_relationships.txt", "w") 
+current_ball_num = 0
 for line in tree_lines:	
-
+	if 'Three Ball Patterns' in line:
+		current_ball_num = 3
+	if 'Four Ball Patterns' in line:
+		current_ball_num = 4
+	if 'Five Ball Patterns' in line:
+		current_ball_num = 5
+	if 'Six Ball Patterns' in line:
+		current_ball_num = 6
 	if "href" in line and "Tricks/" in line and num_parsed_tricks < num_tricks:
 		num_parsed_tricks += 1
 		link = line.split("\"")[1]
@@ -39,24 +47,25 @@ for line in tree_lines:
 		all_tricks[trick_name]["related"] = []
 		all_tricks[trick_name]["url"] = main_url + link
 		all_tricks[trick_name]['name'] = trick_name_with_spaces
+		all_tricks[trick_name]['num'] = current_ball_num
 		next_line_prereq = False
 		next_line_related = False
 		for line in trick_info:
-			print("LINE " , line)
+			#print("LINE " , line)
 			if "Related" in line:
 				next_line_prereq = False
 			if next_line_prereq and "<a" in line:
 				match = re.findall("\">(.+)</a", line)
-				print("MATCH " ,match)
+				#print("MATCH " ,match)
 				if match:
-					print(match[0])
+					#print(match[0])
 					all_tricks[trick_name]['prereqs'].append(match[0])
 					f.write(trick_name + " requires " + match[0] + "\n")
 			if next_line_related and "<a" in line:
 				match = re.findall("\">(.+)</a", line)
-				print("MATCH " ,match)
+				#print("MATCH " ,match)
 				if match:
-					print(match[0])
+					#print(match[0])
 					all_tricks[trick_name]['related'].append(match[0])
 					f.write(trick_name + " related " + match[0] + "\n")
 			if next_line_prereq and "<li" in line:
@@ -66,20 +75,34 @@ for line in tree_lines:
 			if "Prereq" in line:
 				next_line_prereq = True
 				match = re.findall("\">(.+)</a", line)
-				print("MATCH " ,match)
+				#print("MATCH " ,match)
 				if match:
-					print(match[0])
+					#print(match[0])
 					all_tricks[trick_name]['prereqs'].append(match[0])
 					f.write(trick_name + " requires " + match[0] + "\n")
 			if "Related" in line:
 				next_line_related = True
 				#next_line_prereq = False
 				match = re.findall("\">(.+)</a", line)
-				print("MATCH " ,match)
+				#print("MATCH " ,match)
 				if match:
-					print(match[0])
+					#print(match[0])
 					all_tricks[trick_name]['related'].append(match[0])
 					f.write(trick_name + " related " + match[0] + "\n")
 
+all_tricks['FourBallWindmill']["prereqs"] = ['Windmill','Fountain','Four Ball Mills Mess']
+all_tricks['534MillsMess']["prereqs"] = ["Four Ball Mills Mess",'534',"531 Mills Mess"]
+all_tricks['ChopShower']["prereqs"] = ["Windmill",'Crossed-Arm Reverse Cascade']
+all_tricks['ChopShower']["related"] = ["Chops",'Frantic Cascade',"Statue of Liberty"]
+all_tricks['Takearound']["prereqs"] = ["Takeouts",'Flying Disco Drop']
+all_tricks["Shuffler'sMess"]["prereqs"] = ["531",'Mills Mess','531 Mills Mess']
+all_tricks["Levels"]["prereqs"] = ["N-Box","Luke's Shuffle"]
+all_tricks["Frostbite"]["related"] = ["Kraken","Kato's Crux","Shuffler's Mess"]
+all_tricks["FlyingDiscoDrop"]["prereqs"] = ["Shower","Orka's Mess","Hands of Time"]
+all_tricks["TrueBox"]["prereqs"] = ["Box","Inverted Shower"]
+
 f.close()
 print(json.dumps(all_tricks, indent = 4, separators = (",",": ")))
+
+with open('jugglingLibrary.js', 'w') as outfile:  
+    json.dump(all_tricks, outfile)
