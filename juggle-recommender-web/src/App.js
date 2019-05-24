664445e3c926f6055e8cc74ebb1cd3053309e767
ian b <ianboru@gmail.com>
2019-05-23 20:09:52 -0400

    header styles

--- juggle-recommender-web/src/App.js
+++ juggle-recommender-web/src/App.js
@@ -116,9 +116,9 @@ class App extends Component {
 						</div><br/>
 		            </SlidingPane>
 					<div className="header">
-						<span className="title">Juggledex</span><span className="version"> version 0.7</span>
-				            <button className="headerButton" onClick={() => this.setState({ isPaneOpen: true })}>Instructions</button>
-				            <button className="headerButton" onClick={() => this.setState({ isLoginPaneOpen: true })}>Login</button>
+						<span className="title">Juggledex</span><span className="version"> v 0.7</span>
+				        <button className="headerButton" onClick={() => this.setState({ isPaneOpen: true })}>Instructions</button>
+				        <button className="headerButton" onClick={() => this.setState({ isLoginPaneOpen: true })}>Login</button>
 					</div>
 					{!this.state.isPaneOpen && !this.state.isLoginPaneOpen ?
 						<TrickList 
