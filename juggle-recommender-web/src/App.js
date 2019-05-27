d814b7844294c090813629653f29a0507a01b3d9
Timothy Ross <tjthejuggler@gmail.com>
2019-05-26 20:59:25 -0400

    emergency api switchout

--- juggle-recommender-web/src/App.js
+++ juggle-recommender-web/src/App.js
@@ -15,14 +15,14 @@ import firebase from 'firebase'
 
  // Your web app's Firebase configuration
 var firebaseConfig = {
-apiKey: "AIzaSyA_3_UUnQ0iII4jblL4Nf6OLALpH1AbaKQ",
-authDomain: "skilldex-dev-6c0ff.firebaseapp.com",
-databaseURL: "https://skilldex-dev-6c0ff.firebaseio.com",
-projectId: "skilldex-dev-6c0ff",
-storageBucket: "skilldex-dev-6c0ff.appspot.com",
-messagingSenderId: "224766397892",
-appId: "1:224766397892:web:80beef32563065c3"
-};
+    apiKey: "AIzaSyCmnOtb4Wk5MObmo1UPhgEV2Cv3b_6nMuY",
+    authDomain: "skilldex-4ebb4.firebaseapp.com",
+    databaseURL: "https://skilldex-4ebb4.firebaseio.com",
+    projectId: "skilldex-4ebb4",
+    storageBucket: "skilldex-4ebb4.appspot.com",
+    messagingSenderId: "965128070479",
+    appId: "1:965128070479:web:64af3cb91c057166"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
