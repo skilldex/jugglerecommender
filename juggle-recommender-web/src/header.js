c2c301a28cd9e8393c46a7a9bc7de7ffe1257b9d
ian b <ianboru@gmail.com>
2019-07-10 10:31:01 -0400

    home navigation fix

--- juggle-recommender-web/src/header.js
+++ juggle-recommender-web/src/header.js
@@ -24,6 +24,7 @@ class Header extends Component {
 			uiStore.setShowExpandedMenu(false)
 		}else if(paneFlag == "home"){
 			uiStore.setShowHomeScreen(true)
+			uiStore.setDetailTrick(null)
 			if(uiStore.addingTrick){
 			 	uiStore.toggleAddingTrick
 			}
@@ -65,7 +66,7 @@ class Header extends Component {
         		<div>
 	                <div className="header">
 						<div >
-							<span className="title">Juggledex</span><span className="version"> v1.5</span>
+							<span className="title">Juggledex</span><span className="version">v1.6</span>
 					    </div>
 					    <img className="hamburger" onClick={() => uiStore.toggleExpandedMenu()} src={hamburger}/>
 					    
