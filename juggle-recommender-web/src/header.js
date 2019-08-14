56f8101ca4cba0713c523a9d0c92a1456cb362c7
Timothy Ross <tjthejuggler@gmail.com>
2019-08-14 21:27:35 +0100

    db and version

--- juggle-recommender-web/src/header.js
+++ juggle-recommender-web/src/header.js
@@ -114,7 +114,7 @@ class Header extends Component {
         		<div>
 	                <div className="header">
 						<div >
-							<label className="title" onClick={() => this.titleClicked()}>Juggledex</label><span className="version">v1.9</span>
+							<label className="title" onClick={() => this.titleClicked()}>Juggledex</label><span className="version">v2.0</span>
 					    </div>
 					    <img className="hamburger" onClick={() => uiStore.toggleExpandedMenu()} src={hamburger}/>
 					    
