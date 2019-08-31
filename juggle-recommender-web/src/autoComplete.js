cee40e1320a08b018b3998f3abd642295226ac79
Timothy Ross <tjthejuggler@gmail.com>
2019-08-31 12:41:49 +0100

    minor

--- juggle-recommender-web/src/autoComplete.js
+++ juggle-recommender-web/src/autoComplete.js
@@ -22,10 +22,11 @@ class AutoComplete extends Component {
 	 			lowerCaseName = lowerCaseName.slice(0,ballMatch.index)
 	 			name = name.slice(0,ballMatch.index)
 	 		}
-	 		//avoid duplicates
+	 		console.log('matchedNames',matchedNames,name,lowerCaseName,lowerCaseInput)
 	 		if(lowerCaseName.includes(lowerCaseInput) && 
 	 			!matchedNames.includes(lowerCaseName) || 
 	 			this.props.optionsList.length < 4){
+	 			console.log('lessThan4')
 	 			const matchIndex = lowerCaseName.indexOf(lowerCaseInput)
 	 			matchedNames.push(lowerCaseName)
 	 			let optionClassName = "option"
